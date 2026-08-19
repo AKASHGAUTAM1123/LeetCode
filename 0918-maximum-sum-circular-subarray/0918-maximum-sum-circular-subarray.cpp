dbdb25745e66968ed65cf166c51f1a1071c6a0df
AKASHGAUTAM1123 <143429494+AKASHGAUTAM1123@users.noreply.github.com>
2026-08-19 15:58:29 +0530

    Time: 0 ms (100%), Space: 43.7 MB (14.26%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -2,22 +2,25 @@ class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
         int total=0;
+        int n=nums.size();
         int maxending=nums[0];
         int maxsum=nums[0];
         int minending=nums[0];
         int minsum=nums[0];
-
         total+=nums[0];
-        for(int i=1;i<nums.size();i++){
+
+        for(int i=1;i<n;i++){
             total+=nums[i];
-            maxending=max(nums[i],maxending+nums[i]);
+            maxending=max(maxending+nums[i],nums[i]);
             maxsum=max(maxsum,maxending);
 
-            minending=min(nums[i],minending+nums[i]);
+            minending=min(minending+nums[i],nums[i]);
             minsum=min(minsum,minending);
         }
-        if(maxsum<0) return maxsum;
-        int circularsum=total-minsum;
-        return max(maxsum,circularsum);
+
+            if(maxsum<0) return maxsum;
+            int circularsum=total-minsum;
+            return max(maxsum,circularsum);
+        
     }
 };
