1442f9e77bf96b8ed21e4e8aee25a612ac3cec31
AKASHGAUTAM1123 <143429494+AKASHGAUTAM1123@users.noreply.github.com>
2026-08-20 17:40:11 +0530

    Time: 0 ms (100%), Space: 23.2 MB (6.09%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n=intervals.size();
+        vector<vector<int>> temp;
+        vector<vector<int>> res;
+        bool insertd=false;
+        for(int i=0;i<n;i++){
+        if(insertd == false && newInterval[0]<=intervals[i][0]){
+            temp.push_back(newInterval);
+            insertd=true;
+        }
+        temp.push_back(intervals[i]);
+    }
+    if(insertd == false){
+        temp.push_back(newInterval);
+
+    }
+
+        int start1=temp[0][0];
+        int end1=temp[0][1];
+
+        for(int i=1;i<temp.size();i++){
+            int start2=temp[i][0];
+            int end2=temp[i][1];
+
+            if(end1>=start2){
+                start1=start1;
+                end1=max(end1,end2);
+                continue;
+            }
+            res.push_back({start1,end1});
+            start1=start2;
+            end1=end2;
+        }
+        res.push_back({start1,end1});
+        return res;
+        
+    }
+};
