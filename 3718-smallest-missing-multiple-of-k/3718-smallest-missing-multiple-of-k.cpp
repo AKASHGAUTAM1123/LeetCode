ec05ed7354c82cd4d3a14adef722c0b90b504029
AKASHGAUTAM1123 <143429494+AKASHGAUTAM1123@users.noreply.github.com>
2026-08-25 15:07:50 +0530

    Time: 0 ms (100%), Space: 25.1 MB (24.36%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        int n=nums.size();
+        unordered_set<int>s;
+        for(int num:nums){
+            s.insert(num);
+        }
+        int multiple=k;
+        while(s.count(multiple)){
+            multiple+=k;
+        }
+        return multiple;
+    }
+};
