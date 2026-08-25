a818d998caf05e22fa15506daf89ab7c30dadc4f
AKASHGAUTAM1123 <143429494+AKASHGAUTAM1123@users.noreply.github.com>
2026-08-26 00:49:28 +0530

    Time: 7 ms (72.04%), Space: 19.5 MB (60.05%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        if(n<=2) return n;
+        int officer=1;
+        int res=2;
+        int cm=2;
+        while(cm<n){
+            if(nums[cm]==nums[officer-1]&&
+            nums[cm]==nums[officer]){
+                cm++;
+                continue;
+            }
+            nums[officer+1]=nums[cm];
+            officer++;
+            cm++;
+            res++;
+        }
+        return res;
+    }
+};
