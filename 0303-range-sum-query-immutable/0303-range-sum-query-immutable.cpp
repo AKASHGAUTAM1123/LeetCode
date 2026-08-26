5d2c35e79e96589dba49dfbe5aefa74adfaba840
AKASHGAUTAM1123 <143429494+AKASHGAUTAM1123@users.noreply.github.com>
2026-08-26 18:28:44 +0530

    Time: 0 ms (100%), Space: 23.9 MB (62.01%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,12 +1,17 @@
 class NumArray {
 public:
-    vector<int>nums;
+    vector<int>preffix;
     NumArray(vector<int>& nums) {
-        this->nums=nums;
+        int n=nums.size();
+        preffix.resize(n+1,0);
+
+        for(int i=0;i<n;i++){
+            preffix[i+1]=preffix[i]+nums[i];
+        }
     }
     
     int sumRange(int left, int right) {
-        return accumulate(this->nums.begin()+left,this->nums.begin()+right+1,0);
+        return preffix[right+1]-preffix[left];
     }
 };
 
