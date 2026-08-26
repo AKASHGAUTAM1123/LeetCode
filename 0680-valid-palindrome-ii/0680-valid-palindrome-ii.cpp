9da6543fd76ae4e173e9e48765276759c4497f3e
AKASHGAUTAM1123 <143429494+AKASHGAUTAM1123@users.noreply.github.com>
2026-08-26 15:18:58 +0530

    Time: 6 ms (33.97%), Space: 22 MB (77.47%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     bool validPalindrome(string s) {
-       int left = 0,right = s.length() - 1;
-       while(left<right){
-        if(s[left]!=s[right]){
-            return isPalindrome(s,left+1,right)||
-                   isPalindrome(s,left,right-1);
-        }
-        left++;
-        right--;
-       } 
-       return true;
-    }
-
-private:
-    bool isPalindrome(string s,int left,int right){
+        int n=s.size();
+        int left=0;int right=n-1;
         while(left<right){
             if(s[left]!=s[right]){
-                return false;
+                return isPalindrom(s,left+1,right)||
+                        isPalindrom(s,left,right-1);
             }
             left++;
             right--;
         }
         return true;
     }
-
+private:
+    bool isPalindrom(string&s,int left,int right){
+        while(left<right){
+           if(s[left]!=s[right]){
+            return false;
+           } 
+           left++;
+           right--;
+        }
+        return true;
+    }
 };
