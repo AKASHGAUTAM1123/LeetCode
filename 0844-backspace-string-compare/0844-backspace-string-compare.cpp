364b9cad8a2aa8968374f7d6b294093fa2817774
AKASHGAUTAM1123 <143429494+AKASHGAUTAM1123@users.noreply.github.com>
2026-09-01 16:04:33 +0530

    Time: 3 ms (4.76%), Space: 9.7 MB (5.88%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     string process(string s){
-        stack<char>st;
+        stack<int>st;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='#'){
+            if(s[i] == '#'){
                 if(!st.empty()){
                     st.pop();
                 }
             }else{
                 st.push(s[i]);
-            }
+            }   
         }
-            string res="";
-            while(!st.empty()){
-                res+=st.top();
-                st.pop();
-            }
-            reverse(res.begin(),res.end());
-            return res;
+        string res="";
+        while(!st.empty()){
+            res+=st.top();
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
     }
     bool backspaceCompare(string s, string t) {
-        string a=process(s);
-        string b=process(t);
+        string a = process(s);
+        string b = process(t);
+
         return a==b;
     }
 };
