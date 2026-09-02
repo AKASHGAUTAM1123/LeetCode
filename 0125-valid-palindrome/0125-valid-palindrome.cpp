35519ec95ff561728e1da5e89267d25bc5ce2f20
AKASHGAUTAM1123 <143429494+AKASHGAUTAM1123@users.noreply.github.com>
2026-09-02 23:23:38 +0530

    Time: 4 ms (14.09%), Space: 9.8 MB (94.09%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    bool isAlphaNum(char ch){
-        if((ch>= '0' && ch<='9')||
-        (tolower(ch)>='a' && tolower(ch)<='z')){
+    bool isAlphanumeric(char ch){
+        if((ch>='0' && ch<='9')||
+        (tolower(ch) >='a' && tolower(ch)<='z')){
             return true;
-        }        
+        }
         return false;
     }
     bool isPalindrome(string s) {
         int n=s.size();
-        int st=0,end=n-1;
-        while(st<end){
-            if(!isAlphaNum(s[st])){
-                st++;
+        int left=0;int right=n-1;
+        while(left<right){
+            if(!isAlphanumeric(s[left])){
+                left++;
                 continue;
             }
-            if(!isAlphaNum(s[end])){
-                end--;
+            if(!isAlphanumeric(s[right])){
+                right--;
                 continue;
             }
-            if(tolower(s[st])!=tolower(s[end])){
+            if(tolower(s[left])!=tolower(s[right])){
                 return false;
             }
-            st++;
-            end--;
+            left++;
+            right--;
         }
         return true;
+
     }
 };
