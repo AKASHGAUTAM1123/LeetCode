924e4311da00b9e4f0153c025a18c9a6a8fff908
AKASHGAUTAM1123 <143429494+AKASHGAUTAM1123@users.noreply.github.com>
2026-09-05 16:28:57 +0530

    Time: 3 ms (81.44%), Space: 11.5 MB (63.52%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -5,23 +5,21 @@ public:
         int m=p.size();
         vector<int>need(26,0);
         vector<int>have(26,0);
-        vector<int> ans;
-        if(m>n) return ans;
-
+        vector<int>ans;
+        if(m>n)return ans;
         for(char ch:p){
             need[ch-'a']++;
         }
+        int left=0;
+        for(int right=0;right<n;right++){
+            have[s[right] - 'a']++;
 
-        int low=0;
-        for(int high=0;high<n;high++){
-            have[s[high]-'a']++;
-
-            if(high-low+1>m){
-                have[s[low]-'a']--;
-                low++;
+            if(right-left+1>m){
+                have[s[left]-'a']--;
+                left++;
             }
-            if(high-low+1==m&&have==need){
-                ans.push_back(low);
+            if(right-left+1==m&&have==need){
+                ans.push_back(left);
             }
         }
         return ans;
