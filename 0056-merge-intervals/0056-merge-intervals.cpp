7cd2b3e3d706d80722cb45ce2ac3f9349ce5472b
AKASHGAUTAM1123 <143429494+AKASHGAUTAM1123@users.noreply.github.com>
2026-09-15 12:20:11 +0530

    Time: 5 ms (52.3%), Space: 23.8 MB (60.35%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& a) {
-        int n=a.size();
-        vector<vector<int>> res;
+        int n = a.size();
+        vector<vector<int>>res;
         sort(a.begin(),a.end());
         int start1=a[0][0];
         int end1=a[0][1];
-
         for(int i=1;i<n;i++){
             int start2=a[i][0];
             int end2=a[i][1];
-
             if(end1>=start2){
                 start1=start1;
                 end1=max(end1,end2);
