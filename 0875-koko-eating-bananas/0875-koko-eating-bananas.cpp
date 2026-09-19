b2640893586a527d220d258f46b6bf16f67ba30e
AKASHGAUTAM1123 <143429494+AKASHGAUTAM1123@users.noreply.github.com>
2026-09-19 15:40:23 +0530

    Time: 5 ms (78.82%), Space: 22.9 MB (89.88%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    int minEatingSpeed(vector<int>& piles, int h) {
-        int left = 1;
-        int right = *max_element(piles.begin(),piles.end());
-
-        while(left<right){
-            int mid = left + (right - left)/2;
-
-            long long hours = 0;
-
-            for(int pile:piles){
-                hours+=(pile +mid-1)/mid;
-            }
-            if(hours<=h){
-                right = mid;
-            }else{
-                left = mid+1;
+    long long fun(vector<int>&piles,int n,int speed){
+        long long h = 0;
+        for(int i=0;i<n;i++){
+            h += piles[i]/speed;
+            if(piles[i]%speed!=0){
+                h++;
             }
         }
-        return left;
+        return h;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int left=1;int res = INT_MAX;
+        int right=*max_element(piles.begin(),piles.end());
+        while(left<=right){
+        int mid = left+(right-left)/2;
+        long long hour = fun(piles,piles.size(),mid);
+
+        if(hour>h){
+            left=mid+1;
+        }else{
+            res = mid;
+            right=mid-1;
+        }
+    }
+        return res;
     }
 };
